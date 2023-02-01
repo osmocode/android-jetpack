44ab01dee48e73c6322bd8f810184aa50c11c712
osmocode <96887741+osmocode@users.noreply.github.com>
2023-02-01 02:19:28 +0100

    update: experimental preview

--- app/src/main/java/fr/uge/plutus/MainActivity.kt
+++ app/src/main/java/fr/uge/plutus/MainActivity.kt
@@ -3,40 +3,55 @@ package fr.uge.plutus
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import fr.uge.plutus.ui.ant.Ant
+import androidx.navigation.compose.rememberNavController
 import fr.uge.plutus.ui.ant.AntTheme
+import fr.uge.plutus.widget.NavigationBar
+import fr.uge.plutus.widget.NavigationRouter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             AntTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Ant.colors.primary
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar(
+                            navController = navController
+                        )
+                    }
                 ) {
-                    Greeting(name = "Android")
+                    NavigationRouter(
+                        navController = navController
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    widthDp = 412,
+    heightDp = 915
+)
 @Composable
 fun DefaultPreview() {
+    val navController = rememberNavController()
     AntTheme {
-        Greeting("Android")
+        Scaffold(
+            bottomBar = {
+                NavigationBar(
+                    navController = navController
+                )
+            }
+        ) {
+            NavigationRouter(
+                navController = navController
+            )
+        }
     }
 }
