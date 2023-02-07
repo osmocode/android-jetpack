77b2870865280c131db861c66a077812d4a016df
kldufour <kellydufour97@gmail.com>
2023-02-08 00:11:49 +0100

    feat : update transaction pager

--- app/src/main/java/fr/uge/plutus/widget/TransactionPager.kt
+++ app/src/main/java/fr/uge/plutus/widget/TransactionPager.kt
@@ -1,55 +1,50 @@
 package fr.uge.plutus.widget
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import fr.uge.plutus.ui.ant.Ant
 import fr.uge.plutus.ui.components.AntCard
 import fr.uge.plutus.ui.components.AntPagerLayout
 import fr.uge.plutus.ui.components.AntTabBarItem
-
-sealed class TransactionPagerItem(
-    label: String,
-    content: @Composable (page: Int) -> Unit
-): AntTabBarItem(
-    label,
-    content
-) {
-    object Past: TransactionPagerItem(
-        label = "Past",
-        content = { page ->
-            LazyColumn{
-                items(10){
-                    AntCard(title = "Crous", description = "07 fév 2023", extras = "3,25€")
-                }
-            }
-        }
-    )
-
-    object Coming: TransactionPagerItem(
-        label = "Coming",
-        content = { page ->
-            Box {
-                Text(
-                    text = "Page: $page",
-                    modifier = Modifier.align(Alignment.Center)
-                )
-            }
-        }
-    )
-}
 
 @Composable
 fun TransactionPager(
     navController: NavHostController
 ) {
     val items = listOf(
-        TransactionPagerItem.Past,
-        TransactionPagerItem.Coming
+        AntTabBarItem (
+            label = "Past",
+            content = { page ->
+                Box(modifier = Modifier
+                    .padding(
+                        top = 10.dp,
+                        start = 10.dp,
+                        end = 10.dp))
+                LazyColumn{
+                    items(10){
+                        AntCard(title = "Crous", description = "07 fév 2023", extras = "3,25€")
+                    }
+                    item { Spacer(modifier = Modifier.size(150.dp)) }
+                }
+            }
+    ),
+        AntTabBarItem(
+            label = "Coming",
+            content = { page ->
+                Box {
+                    Text(
+                        text = "Page: $page",
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+            }
+        )
     )
 
     AntPagerLayout(
