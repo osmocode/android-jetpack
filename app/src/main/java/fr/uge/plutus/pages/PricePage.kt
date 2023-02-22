97fb3f682e894cd4f65681f9e90bc2bdd5b0f831
kldufour <kellydufour97@gmail.com>
2023-02-22 19:11:20 +0100

    feat : create PricePage

--- app/src/main/java/fr/uge/plutus/pages/PricePage.kt
+++ app/src/main/java/fr/uge/plutus/pages/PricePage.kt
@@ -1,21 +1,40 @@
 package fr.uge.plutus.pages
 
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.ChevronRight
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import fr.uge.plutus.ui.components.AntActionButton
-import fr.uge.plutus.ui.components.AntBottomSheetScaffold
-import fr.uge.plutus.ui.components.AntTopBar
+import androidx.navigation.compose.rememberNavController
+import fr.uge.plutus.data.model.Price
+
+import fr.uge.plutus.ui.components.*
 
 @Composable
 fun PricePage(
-    navController: NavHostController
+    navController: NavHostController,
+    price : Price,
+    onSubmit : (Double) -> Unit
 ) {
     val sheetVisible = remember { mutableStateOf(false) }
+
+    val prefix = remember { mutableStateOf(price.currency) }
+    val amount = remember { mutableStateOf(price.amount) }
+
+    val buttonDisable = remember { mutableStateOf(true) }
+
+    val focusManger = LocalFocusManager.current
 
     AntBottomSheetScaffold(
         topBar = {
@@ -35,6 +54,59 @@ fun PricePage(
         sheetVisible = sheetVisible,
         sheetContent = {}
     ) {
-
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .pointerInput(Unit){
+                    detectTapGestures(onTap = {
+                        focusManger.clearFocus()
+                    })
+                },
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Box{}
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CustomButton(
+                    title = "Currency",
+                    type = CustomButtonType.LINK,
+                    leadingIcon = Icons.Outlined.ChevronRight,
+                    onClick = {}
+                )
+                AntAmountField(
+                    prefix = prefix,
+                    amount = amount
+                )
+            }
+            CustomButton(
+                title = "Submit",
+                type = CustomButtonType.PRIMARY,
+                disabled = buttonDisable,
+                onClick = {
+                    onSubmit(amount.value)
+                    focusManger.clearFocus()
+                }
+            )
+        }
     }
+
+    LaunchedEffect(key1 = amount.value){
+        buttonDisable.value = price.amount == amount.value
+    }
+}
+
+
+@Preview
+@Composable
+fun PricePagePreview(
+
+) {
+    val navController = rememberNavController()
+
+    PricePage(
+        navController = navController,
+        price = Price("$", 0.0)
+    ){}
 }
