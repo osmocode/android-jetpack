192a6a22e36ca8f8b4e5f40ecf35f24063de0cb6
osmocode <96887741+osmocode@users.noreply.github.com>
2023-02-23 19:14:48 +0100

    feat: init wallet repository

---
+++ app/src/main/java/fr/uge/plutus/data/repository/WalletRepository.kt
@@ -0,0 +1,26 @@
+package fr.uge.plutus.data.repository
+
+import fr.uge.plutus.data.dao.WalletDao
+import fr.uge.plutus.data.interfaces.IWalletRepository
+import fr.uge.plutus.data.model.Wallet
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class WalletRepository @Inject constructor(
+    private val walletDao: WalletDao
+) : IWalletRepository {
+    override fun retrieveAllWallet(): Flow<List<Wallet>> = walletDao.retrieveAll()
+
+    override suspend fun createWallet(wallet: Wallet) =
+        walletDao.create(wallet)
+
+    override suspend fun retrieveWallet(id: Int): Wallet? =
+        walletDao.retrieveById(id)
+
+    override suspend fun updateWallet(wallet: Wallet): Int =
+        walletDao.update(wallet)
+
+    override suspend fun deleteWallet(wallet: Wallet): Int =
+        walletDao.delete(wallet)
+
+}
