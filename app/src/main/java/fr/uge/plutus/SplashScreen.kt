ebb9c3c587207926aa732202bc4fa1fa14591067
kldufour <kellydufour97@gmail.com>
2023-02-26 19:55:53 +0100

    update : Add Spinner and Plutus on the SplashScreen

--- app/src/main/java/fr/uge/plutus/SplashScreen.kt
+++ app/src/main/java/fr/uge/plutus/SplashScreen.kt
@@ -1,16 +1,22 @@
 package fr.uge.plutus
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import fr.uge.plutus.ui.ant.Ant
 
 
 @Composable
@@ -18,15 +24,35 @@ fun SplashScreen(
     isLoading: MutableState<Boolean>,
     navController: NavHostController
 ) {
-    Box(
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.LightGray)
+            .background(color = Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Loading...",
+            text = "Plutus",
             fontSize = 50.sp,
-            modifier = Modifier.align(Alignment.Center)
+            color = Ant.colors.primary_text
         )
+        Spacer(modifier = Modifier.padding(Ant.spacing.default))
+        CircularProgressIndicator(
+            modifier = Modifier.size(100.dp),
+            strokeWidth = 8.dp,
+            color = Ant.colors.primary_color_5
+        )
+
     }
+}
+
+
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    val navController = rememberNavController()
+    val isLoading = remember { mutableStateOf(true) }
+
+    SplashScreen(isLoading = isLoading, navController = navController)
 }
